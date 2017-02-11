278dde6bd50a5e3328b6faef2a35da17cead6c6e
Rafael Lorandi <coolparadox@gmail.com>
2017-02-11 19:57:23 -0200

    Testing for generation of 400 Bad Request HTTP status response

--- src/main/java/mars/RobotController.java
+++ src/main/java/mars/RobotController.java
@@ -1,5 +1,7 @@
 package mars;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -9,11 +11,6 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class RobotController {
 
-    @PostMapping("/rest/mars/{command}")
-    public @ResponseBody Robot robotPostCommand(@PathVariable(value="command") final String command) {
-        return new Robot(command);
-    }
-
     @PostMapping("/rest/mars")
     public @ResponseBody Robot robotPostEmpty() {
         return new Robot("");
@@ -22,6 +19,14 @@ public class RobotController {
     @GetMapping("/rest/mars")
     public @ResponseBody Robot robotGetEmpty() {
         return new Robot("");
+    }
+
+    @PostMapping("/rest/mars/{command}")
+    public @ResponseBody ResponseEntity<Robot> robotPostCommand(@PathVariable(value="command") final String command) {
+		if (command.equals("X"))
+			return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(null);
+		else
+			return ResponseEntity.ok(new Robot(command));
     }
 
 }
