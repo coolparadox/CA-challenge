bdd6e6589bc487de7ecf87e4f48f18c0c9059cc0
Rafael Lorandi <coolparadox@gmail.com>
2017-02-11 15:24:41 -0200

    Replacing name parameter by name path variable

--- src/main/java/hello/GreetingController.java
+++ src/main/java/hello/GreetingController.java
@@ -1,8 +1,9 @@
 package hello;
 
 import java.util.concurrent.atomic.AtomicLong;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -11,8 +12,8 @@ public class GreetingController {
     private static final String template = "Hello, %s!";
     private final AtomicLong counter = new AtomicLong();
 
-    @RequestMapping("/greeting")
-    public Greeting greeting(@RequestParam(value="name", defaultValue="World") String name) {
+    @RequestMapping("/greeting/{name}")
+    public @ResponseBody Greeting greeting(@PathVariable(value="name") final String name) {
         return new Greeting(counter.incrementAndGet(),
                             String.format(template, name));
     }
