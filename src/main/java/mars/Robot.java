4a488d43075db77f225a64a3e735f17b51620b8b
Rafael Lorandi <coolparadox@gmail.com>
2017-02-12 18:10:22 -0200

    Robot's Orientation enumeration

--- src/main/java/mars/Robot.java
+++ src/main/java/mars/Robot.java
@@ -2,28 +2,134 @@ package mars;
 
 public class Robot {
 
+	// Hardcoded grid limits
+	static final int minX = 0;
+	static final int minY = 0;
+	static final int maxX = 5;
+	static final int maxY = 5;
+
+	// State variables
 	private boolean mIsLost;
 	private int mPosX;
 	private int mPosY;
 	private Orientation mOrientation;
 
+	// Orientation objects.
+	// These perform robot's movement and turning.
 	private enum Orientation {
 
 		NORTH {
-		},
 
-		SOUTH {
+			// Walk North
+			@Override
+			public void stepForward(Robot robot) {
+				if (robot.mPosY < maxY)
+					robot.mPosY++;
+				else
+					// Uncharted territory
+					robot.mIsLost = true;
+			}
+
+			// Face West
+			@Override
+			public void turnLeft(Robot robot) {
+				robot.mOrientation = WEST;
+			}
+
+			// Face East
+			@Override
+			public void turnRight(Robot robot) {
+				robot.mOrientation = EAST;
+			}
+
 		},
 
 		EAST {
+
+			// Walk East
+			@Override
+			public void stepForward(Robot robot) {
+				if (robot.mPosX < maxX)
+					robot.mPosX++;
+				else
+					// Uncharted territory
+					robot.mIsLost = true;
+			}
+
+			// Face North
+			@Override
+			public void turnLeft(Robot robot) {
+				robot.mOrientation = NORTH;
+			}
+
+			// Face South
+			@Override
+			public void turnRight(Robot robot) {
+				robot.mOrientation = SOUTH;
+			}
+
+		},
+
+		SOUTH {
+
+			// Walk South
+			@Override
+			public void stepForward(Robot robot) {
+				if (robot.mPosY > minY)
+					robot.mPosY--;
+				else
+					// Uncharted territory
+					robot.mIsLost = true;
+			}
+
+			// Face East
+			@Override
+			public void turnLeft(Robot robot) {
+				robot.mOrientation = EAST;
+			}
+
+			// Face West
+			@Override
+			public void turnRight(Robot robot) {
+				robot.mOrientation = WEST;
+			}
+
 		},
 
 		WEST {
+
+			// Walk West
+			@Override
+			public void stepForward(Robot robot) {
+				if (robot.mPosX > minX)
+					robot.mPosX--;
+				else
+					// Uncharted territory
+					robot.mIsLost = true;
+			}
+
+			// Face South
+			@Override
+			public void turnLeft(Robot robot) {
+				robot.mOrientation = SOUTH;
+			}
+
+			// Face North
+			@Override
+			public void turnRight(Robot robot) {
+				robot.mOrientation = NORTH;
+			}
+
 		};
 
-		abstract public void move(Robot parent);
-		abstract public void left(Robot parent);
-		abstract public void right(Robot parent);
+		// Make robot take a step forward
+		abstract public void stepForward(Robot robot);
+
+		// Make robot turn left by 90 degrees
+		abstract public void turnLeft(Robot robot);
+
+		// Make robot turn right by 90 degrees
+		abstract public void turnRight(Robot robot);
 
 	}
 
